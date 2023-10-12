cd493d120062e03dd591b107492bc72d0caaeece
Ujjwal Giri <ug.online.st@gmail.com>
2023-10-12 12:13:11 +0545

    CORS Changes

--- api/app.js
+++ api/app.js
@@ -14,11 +14,25 @@ connect();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "https://bookstore-backend-bice.vercel.app",
+      "http://localhost:3000",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.use(fileUpload());
 app.use(cookieParser());
 app.use(express.json());
